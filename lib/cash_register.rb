34311ad9f7802813e11b68730634a67f67292fd5
Kyle Luke <th.oldschool@gmail.com>
2019-09-05 03:42:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,13 @@
 class CashRegister
   attr_accessor :total, :discount
-  
+  CART = {}
   def initialize(discount = nil)
     @total = 0 
     @discount = discount
+  end
+  
+  def add_item(title, price)
+    CART[item] = price
   end
   
 end
